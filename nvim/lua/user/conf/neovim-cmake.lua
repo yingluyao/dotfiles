a444c58cc742154e745a2688885451f4ba04dd65
yingly <luyao.ying@hotmail.com>
2022-08-13 17:42:38 +0800

    set sample path for neovim cmake

--- nvim/lua/user/conf/neovim-cmake.lua
+++ nvim/lua/user/conf/neovim-cmake.lua
@@ -27,7 +27,7 @@ cmake.setup({
   cmake_executable = 'cmake', -- CMake executable to run.
   parameters_file = 'neovim.json', -- JSON file to store information about selected target, run arguments and build type.
   build_dir = tostring(Path:new('{cwd}', 'build', '{os}-{build_type}')), -- Build directory. The expressions `{cwd}`, `{os}` and `{build_type}` will be expanded with the corresponding text values.
-  -- samples_path = tostring(script_path:parent():parent():parent() / 'samples'), -- Folder with samples. `samples` folder from the plugin directory is used by default.
+  samples_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/neovim-cmake/samples', -- Folder with samples. `samples` folder from the plugin directory is used by default.
   default_projects_path = tostring(Path:new(vim.loop.os_homedir(), 'Projects')), -- Default folder for creating project.
   configure_args = { '-D', 'CMAKE_EXPORT_COMPILE_COMMANDS=1' }, -- Default arguments that will be always passed at cmake configure step. By default tells cmake to generate `compile_commands.json`.
   build_args = {
