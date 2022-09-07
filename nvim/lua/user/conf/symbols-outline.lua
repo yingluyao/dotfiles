5a1c6e4abdc997bc5a6e834fdc11d12d7899fd6f
yingly <luyao.ying@hotmail.com>
2022-09-07 13:05:25 +0800

    fix symbol-outline error

--- nvim/lua/user/conf/symbols-outline.lua
+++ nvim/lua/user/conf/symbols-outline.lua
@@ -1,5 +1,12 @@
 -- https://github.com/simrat39/symbols-outline.nvim
-vim.g.symbols_outline = {
+
+local status_ok, outline = pcall(require, "symbols-outline")
+if not status_ok then
+  vim.notify("symbols-outline not found!")
+  return
+end
+
+local opts = {
     highlight_hovered_item = true,
     show_guides = true,
     auto_preview = false,
@@ -11,6 +18,10 @@ vim.g.symbols_outline = {
     show_relative_numbers = false,
     show_symbol_details = true,
     preview_bg_highlight = 'Pmenu',
+    autofold_depth = nil,
+    auto_unfold_hover = true,
+    fold_markers = { '', '' },
+    wrap = false,
     keymaps = { -- These keymaps can be a string or a table for multiple keys
         close = {"<Esc>", "q"},
         goto_location = "<Cr>",
@@ -19,6 +30,11 @@ vim.g.symbols_outline = {
         toggle_preview = "K",
         rename_symbol = "r",
         code_actions = "a",
+        fold = "h",
+        unfold = "l",
+        fold_all = "W",
+        unfold_all = "E",
+        fold_reset = "R",
     },
     lsp_blacklist = {},
     symbol_blacklist = {},
@@ -51,3 +67,5 @@ vim.g.symbols_outline = {
         TypeParameter = {icon = "𝙏", hl = "TSParameter"}
     }
 }
+
+outline.setup(opts)
